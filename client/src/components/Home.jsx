6949359feecdf6f4bbfad0f5ffc163df8ee2ca27
samthatcode <samthatcode@gmail.com>
2023-10-03 15:45:57 +0100

    Bug fix

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div> 
       <Layout>
         <Hero />
-        <ProductPage />        
+        {/* <ProductPage />         */}
         <ContactUs />     
       </Layout>  
     </div>
